e722e7fd8363868ab0c015e18cda8d700a1d009f
Carlos Riojas <carlos.riojas88@gmail.com>
2022-07-27 10:15:41 -0500

    first commit

---
+++ src/main/java/com/UDNDcourse1/springbootcourse/UdnDcourse1Application.java
@@ -0,0 +1,13 @@
+package com.UDNDcourse1.springbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdnDcourse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdnDcourse1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/UDNDcourse1/springbootcourse/UdnDcourse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UDNDcourse1.springbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdnDcourse1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
