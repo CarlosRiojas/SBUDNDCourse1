5254f2d2b1b24b09818f40712f479689d9042ea1
CarlosRiojas <carlos.riojas88@gmail.com>
2022-07-28 16:25:16 -0500

    Second Init commit "HELLO"

---
+++ src/main/java/com/UDNDcourse1/springbootcourse/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.UDNDcourse1.springbootcourse.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String getHomePage(){
+        return "home";
+    }
+
+}
