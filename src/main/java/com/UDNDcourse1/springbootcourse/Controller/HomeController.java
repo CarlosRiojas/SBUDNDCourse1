838b379210a81fd4a8881f9f8f75deeaaf7abcda
Carlos Riojas <carlos.riojas88@gmail.com>
2022-07-29 10:15:00 -0500

    thymeleaf ex

--- src/main/java/com/UDNDcourse1/springbootcourse/Controller/HomeController.java
+++ src/main/java/com/UDNDcourse1/springbootcourse/Controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.UDNDcourse1.springbootcourse.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String getHomePage(){
+    public String getHomePage(Model model){
+        model.addAttribute("greetings", new String[]{"Hi","Hello","goodbye"});
         return "home";
     }
 
